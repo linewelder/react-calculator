4deb72c070c0eb2445d3989b317aaf39c2101d7c
linewelder <redalerm@gmail.com>
2024-08-08 13:38:45 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import '@fontsource-variable/rubik';
+
+const buttons = [
+    'C', '←', '⚠', '÷',
+    '7', '8', '9', '×',
+    '4', '5', '6', '-',
+    '1', '2', '3', '+',
+    '0', '.', '=',
+];
+
+const operatorMap: { [key: string]: string } = {
+    '×': '*',
+    '÷': '/',
+};
+
+const keyboardMap: { [key: string]: string } = {
+    'Enter': '=',
+    '*': '×',
+    '/': '÷',
+    'Backspace': '←',
+    'Delete': 'C',
+};
+
+const calculateAnswer = (text: string): string => {
+    try {
+        let expression = '';
+        for (const char of text) {
+            expression += operatorMap[char] ?? char;
+        }
+        return '' + (eval(expression) ?? '');
+    } catch (exception) {
+        return 'Error!';
+    }
+};
+
+export default function App() {
+    const [text, setText] = useState("");
+
+    const getActionForButton = (button: string) => {
+        if (button === 'C') {
+            return () => setText('');
+        } else if (button === '=') {
+            return () => setText(calculateAnswer);
+        } else if (button === '←') {
+            return () => setText(text => text.substring(0, text.length - 1));
+        } else if (button === ' ') {
+            return () => {};
+        } else if (button === '⚠') {
+            return () => setText('alert(1)');
+        } else {
+            return () => setText(text => text + button);
+        }
+    };
+
+    const onKeyDown = (e: KeyboardEvent) => {
+        const button = keyboardMap[e.key] ?? e.key;
+        if (buttons.includes(button)) {
+            getActionForButton(button)();
+            e.stopPropagation();
+            e.preventDefault();
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('keydown', onKeyDown);
+        return () => window.removeEventListener('keydown', onKeyDown);
+    }, []);
+
+    return (
+        <>
+            <div className="calculator">    
+                <input className="output-display" disabled value={text} placeholder="0"/>
+                <div className="buttons">
+                    {buttons.map(value =>
+                        <button onClick={getActionForButton(value)} key={value} data-value={value}>
+                            {value}
+                        </button>
+                    )}
+                </div>
+            </div>
+        </>
+    );
+}
